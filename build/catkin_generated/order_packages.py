08f50685da9e7411d2596961861b772b1bd57e33
Ruturaj Sambhus <ruturajsambhus@vt.edu>
2022-07-11 16:30:08 -0400

    Removedhelloworld which failed

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/trec/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/trec/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/trec/catkin_ws/devel;/opt/ros/noetic' != '' else []
